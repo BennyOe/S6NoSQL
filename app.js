1ba1c0884280efa0aa3c9036aa9ff0b2b4c85e2b
BennyOe <b.oechsle@web.de>
2021-10-26 07:13:45 +0200

    style

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const app = Vue.createApp({
             loc: "",
             pop: "",
             state: "",
-            error: "",
+            cityError: "",
+            plzError: "",
             zipCodes: [],
         };
     },
@@ -25,13 +26,15 @@ const app = Vue.createApp({
                     .then((response) => {
                         console.log(response);
                         if (response.data.obj !== null) {
-                            this.error = "";
+                            this.plzError = "";
                             this.city = response.data.obj.city;
                             this.state = response.data.obj.state;
+                            this.pop = response.data.obj.pop;
+                            this.loc = response.data.obj.loc;
                         } else {
                             this.city = "";
                             this.state = "";
-                            this.error = "No entry found for this zip-code";
+                            this.plzError = "No entry found for this zip-code";
                         }
                     });
             }
@@ -50,13 +53,16 @@ const app = Vue.createApp({
                     )
                     .then((response) => {
                         console.log(response);
-                        if (response.data.obj !== null) {
-                            this.error = "";
+                        if (response.data.obj.length != 0) {
+                            this.cityError = "";
                             this.zipCodes = response.data.obj;
                             this.plz = this.zipCodes[0];
+                            this.state = "";
+                            this.pop = "";
+                            this.loc = "";
                         } else {
                             this.zipCodes = [];
-                            this.error = "No entry found for this city";
+                            this.cityError = "No entry found for this city";
                         }
                     });
             }
